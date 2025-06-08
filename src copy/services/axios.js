3a0391ce7c7776b0ba9cde2f92712a61eff006bf
Whiz225 <nwosuemmanuel1994@gmail.com>
2025-06-08 15:08:53 +0100

    axios.js update4

--- src copy/services/axios.js
+++ src copy/services/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // ✅ Make sure it's localhost not 127.0.0.1
   // baseURL: "https://booking-app-api.onrender.com/api/v1",
-  baseURL: "https://booking-app-api-tp56.onrender.com",
+  baseURL: "https://booking-app-api-tp56.onrender.com/api/v1",
   // ✅ Needed for sending cookies
   withCredentials: true,
 });
