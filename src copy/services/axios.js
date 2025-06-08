42b4bb5597a8b58b962d9cb042bb042ff7beac08
Whiz225 <nwosuemmanuel1994@gmail.com>
2025-06-08 11:00:12 +0100

    axios.js modified

--- src copy/services/axios.js
+++ src copy/services/axios.js
@@ -1,11 +1,10 @@
-
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // ✅ Make sure it's localhost not 127.0.0.1
-  baseURL: "http://localhost:8000/api/v1", 
+  // ✅ Make sure it's localhost not 127.0.0.1
+  baseURL: "https://localhost:8000/api/v1",
   // ✅ Needed for sending cookies
-  withCredentials: true, 
+  withCredentials: true,
 });
 
 export default axiosInstance;
